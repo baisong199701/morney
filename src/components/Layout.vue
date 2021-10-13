aed3653a92c6f242b09f3aecea5fad2b7137fcd0
liubaisong <1174306192@qq.com>
2021-10-13 17:19:04 +0800

    把Money.vue分成四个子组件

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,20 +1,27 @@
 <template>
-   <div class="nav-wrapper">
-    <div class="content">
-           <slot/>
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
+      <slot />
     </div>
- <Nav />
-    </div>
+    <Nav />
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  props: ["classPrefix"],
+  name: "Layout",
+};
+</script>
+
 <style lang="scss" scoped>
-.nav-wrapper{
-    border:1px solid green;
-    display:flex;
-    flex-direction: column;
-    height:100vh;
+.layout-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
-.content{
-    overflow:auto;
-    flex-grow:1;
+.content {
+  overflow: auto;
+  flex-grow: 1;
 }
 </style>
