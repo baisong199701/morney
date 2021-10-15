8148da4671a0cbfba5bf0b8cde42069b2fc829b2
liubaisong <1174306192@qq.com>
2021-10-15 14:38:40 +0800

    完成types组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "Types",
-};
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Type extends Vue {
+  type = "-"; //'-'表示支出，'+'表示收入
+
+  selectType(type: string) {
+    // type只能是'-'和'+'中的一个
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unknow");
+    }
+    this.type = type;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
