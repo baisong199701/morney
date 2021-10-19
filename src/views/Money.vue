e96f545dbca37c35d1d262d2e68baa96a2f63e12
liubaisong <1174306192@qq.com>
2021-10-19 21:41:52 +0800

    解决id生成为题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,13 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
-    <Notes @update:value="onUpdateNotes" />
+    <div class="notes">
+      <FormItem
+        field-name="备注"
+        placeholder="在这里输入备注"
+        @update:value="onUpdateNotes"
+      />
+    </div>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
@@ -11,26 +17,20 @@
 import Vue from "vue";
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
-import Notes from "@/components/Money/Notes.vue";
+import FormItem from "@/components/Money/FormItem.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
-const recordList: Record[] = JSON.parse(
-  window.localStorage.getItem("recordList") || "[]"
-);
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number; // 数据类型 object | string
-  createdAt?: Date; // 类 / 构造函数
-};
+import recordListModel from "@/models/recordListModel";
+import tagListModel from "@/models/tagListModel";
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 @Component({
-  components: { Tags, Notes, Types, NumberPad },
+  components: { Tags, FormItem, Types, NumberPad },
 })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行", "彩票"];
-  recordList: Record[] = recordList;
-  record: Record = {
+  tags = tagList;
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags: [],
     notes: "",
     type: "-",
@@ -43,13 +43,13 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
   onRecordListChange() {
-    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+    recordListModel.save(this.recordList);
   }
 }
 </script>
@@ -58,5 +58,8 @@ export default class Money extends Vue {
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
+}
+.notes {
+  padding: 12px 0;
 }
 </style>
