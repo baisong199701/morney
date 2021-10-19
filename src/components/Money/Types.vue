6d60368e62c4451ca50568af30f470eca95463bf
liubaisong <1174306192@qq.com>
2021-10-19 13:44:19 +0800

    添加createdAt字段

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
-      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+      <li :class="value === '-' && 'selected'" @click="selectType('-')">
+        支出
+      </li>
+      <li :class="value === '+' && 'selected'" @click="selectType('+')">
+        收入
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
-
+import { Component, Prop, Watch } from "vue-property-decorator";
 @Component
-export default class Type extends Vue {
-  type = "-"; //'-'表示支出，'+'表示收入
-
+export default class Types extends Vue {
+  @Prop() readonly value!: string;
   selectType(type: string) {
-    // type只能是'-'和'+'中的一个
     if (type !== "-" && type !== "+") {
-      throw new Error("type is unknow");
+      throw new Error("type is unknown");
     }
-    this.type = type;
+    this.$emit("update:value", type);
   }
 }
 </script>
